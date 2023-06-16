8bac709fdaf6e3f039f03887a67ae6981452ba52
Matheus Bonnet <matheusbonnet2019@gmail.com>
2023-06-16 00:52:18 -0300

    Gerando url pre assinada da foto do usuario e dos produtos para o front consumir

--- src/main/java/com/br/tcc/bfn/controllers/S3Controller.java
+++ src/main/java/com/br/tcc/bfn/controllers/S3Controller.java
@@ -1,8 +1,7 @@
 package com.br.tcc.bfn.controllers;
 
 import com.br.tcc.bfn.dtos.Response;
-import com.br.tcc.bfn.dtos.UserDTO;
-import com.br.tcc.bfn.models.Product;
+import com.br.tcc.bfn.exceptions.ProductNotFoundException;
 import com.br.tcc.bfn.services.S3Service;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,11 +9,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/s3")
@@ -72,17 +77,21 @@ public class S3Controller {
     }
 
     @GetMapping("/getObjectUserProfile/{id}")
-    public ResponseEntity<Response<Void>> getObjectBucketUser(@PathVariable final Long id){
-        Response<Void> dtoResponse = new Response<>();
-        try{
-            s3Service.getObjectOnBucktUser(id);
-            dtoResponse.setStatusCode(HttpStatus.OK.value());
-            return ResponseEntity.ok().body(dtoResponse);
+    public ResponseEntity<String> getObjectBucketUser(@PathVariable final Long id) throws Exception {
+        try {
+            return ResponseEntity.ok().body(s3Service.getObjectOnBucktUser(id));
         }catch (Exception e){
-            dtoResponse.setStatusCode(HttpStatus.NOT_FOUND.value());
-            dtoResponse.setError(e.getMessage());
-            return ResponseEntity.badRequest().body(dtoResponse);
+            return ResponseEntity.internalServerError().body(e.getMessage());
         }
-
     }
+
+    @GetMapping("/getObjectProductImage/{id}")
+    public ResponseEntity<List<String>> getObjectBucketProduct(@PathVariable final Long id) throws Exception {
+        try {
+            return ResponseEntity.ok().body(s3Service.getObjectOnBucketProductImage(id));
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(Collections.singletonList(e.getMessage()));
+        }
+    }
+
 }
