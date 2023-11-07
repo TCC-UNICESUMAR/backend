916fce50d79e6a344465a708249bd1171b9f78dd
MatheusBonnet <matheusbonnet2019@gmail.com>
2023-11-07 14:40:17 -0300

    Ajuste no populator do user

--- src/main/java/com/br/tcc/bfn/populators/UserPopulator.java
+++ src/main/java/com/br/tcc/bfn/populators/UserPopulator.java
@@ -1,26 +1,44 @@
 package com.br.tcc.bfn.populators;
 
 import com.br.tcc.bfn.dtos.RegisterRequest;
+import com.br.tcc.bfn.models.Address;
 import com.br.tcc.bfn.models.User;
-import com.br.tcc.bfn.repositories.RoleRepository;
+import com.br.tcc.bfn.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserPopulator implements Populator<User, RegisterRequest> {
 
-    private final PasswordEncoder passwordEncoder;
-    private final RoleRepository roleRepository;
-
-    public UserPopulator(PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
-        this.passwordEncoder = passwordEncoder;
-        this.roleRepository = roleRepository;
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private CityRepository cityRepository;
+    @Autowired
+    private StateRepository stateRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AddressRepository addressRepository;
 
     @Override
     public void populate(User user, RegisterRequest request) {
         user.setName(request.getName());
         user.setEmail(request.getEmail());
         user.setCpfOrCnpj(request.getCnpjOrCpf());
+        user.setPhone(request.getPhone());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        Address address = user.getAddress();
+        if (!address.getCity().getCityName().equals(request.getAddress().getCity())){
+            address.setCity(cityRepository.findByCityName(request.getAddress().getCity()));
+            addressRepository.save(address);
+        }
+        if (!address.getState().getStateName().equals(request.getAddress().getUf())){
+            address.setState(stateRepository.findStateByUf(request.getAddress().getUf()));
+            addressRepository.save(address);
+        }
     }
 }
